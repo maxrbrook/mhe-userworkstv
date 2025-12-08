3a4326e1c8807b42e930f805d3b6b933e97257de
Max Brook <maxrbrook00@hotmail.com>
2025-12-08 14:15:07 +1100

    updates to the Install script to correctly write user and group to the service unit

--- installscript.sh
+++ installscript.sh
@@ -10,9 +10,9 @@ npm install dotenv express axios ejs
 
 cd scripts
 # Add required information into the service script
-sed -i "7i\\User="$USER worktvstart.service
-sed -i "8i\\Group="$USER worktvstart.service
-sed -i "9i\\WorkingDirectory=/home/"$USER"/mhe-qrprinter/src" worktvstart.service
+sed -i "7i\\User="$USER userworkstv.service
+sed -i "8i\\Group="$USER userworkstv.service
+sed -i "9i\\WorkingDirectory=/home/"$USER"/mhe-userworkstv/src" userworkstv.service
 
 echo '>> MOVING SCRIPTS'
 if [ "$(id -u)" -ne 0 ]; then
@@ -20,8 +20,8 @@ if [ "$(id -u)" -ne 0 ]; then
   SUDO='sudo'
 fi
 
-$SUDO cp -f worktvautostart.sh /usr/local/bin
-$SUDO cp -f worktvstart.service /etc/systemd/system
-$SUDO systemctl enable --now worktvstart.service
+$SUDO cp -f userworkstvlaunch.sh /usr/local/bin
+$SUDO cp -f userworkstv.service /etc/systemd/system
+$SUDO systemctl enable --now userworkstv.service
 
 echo '>> INSTALL SUCCESSFUL'
